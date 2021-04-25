4dca05e1fb9a04e28cef56cd17fbf0927437c50c
Krzysztof Łukasik <lukasik.krzysztof@gmail.com>
2021-04-25 15:13:40 +0200

    Update labels

--- src/calculator/components/Calculator/CalculatorSummaryBusinessTotal.jsx
+++ src/calculator/components/Calculator/CalculatorSummaryBusinessTotal.jsx
@@ -4,35 +4,41 @@ import PropTypes from 'prop-types';
 
 import {Spin} from 'antd';
 
-import {PrintAsset,
-  PrintGainLossLabel
-} from '../Print';
+import {PrintAsset, PrintGainLossLabel} from '../Print';
 import InfoItem from '../Misc/InfoItem';
 
 class CalculatorSummary extends React.Component {
   static propTypes = {
     calculator: PropTypes.object,
-    colors: PropTypes.bool
-  }
+    colors: PropTypes.bool,
+  };
 
   render() {
     const {calculator} = this.props;
     const lastOperation = calculator.getSummaryOperation();
     const calculatorStep = lastOperation.calculatorStep;
     if (!calculatorStep) {
-      return <div className="empty-box"><Spin /></div>;
+      return (
+        <div className="empty-box">
+          <Spin />
+        </div>
+      );
     }
     const fieldsRequired = [
       'residenceCurrency',
       'sumProceeds',
       'sumCostBasis',
-      'sumGainLoss'
+      'sumGainLoss',
     ];
     let i;
     const errors = [];
     for (i = 0; i < fieldsRequired.length; i += 1) {
       if (calculatorStep[fieldsRequired[i]] === undefined) {
-        errors.push(<div key={`${fieldsRequired[i]}-required`}>no calculatorStep.{fieldsRequired[i]}</div>);
+        errors.push(
+          <div key={`${fieldsRequired[i]}-required`}>
+            no calculatorStep.{fieldsRequired[i]}
+          </div>,
+        );
       } else {
         // console.log(`calculatorStep[${fieldsRequired[i]}]`, calculatorStep[fieldsRequired[i]]);
       }
@@ -43,20 +49,25 @@ class CalculatorSummary extends React.Component {
     const colors = this.props.colors;
     const formItemLayout = {
       labelCol: {span: 10},
-      wrapperCol: {span: 6}
+      wrapperCol: {span: 6},
     };
     return (
-      <div>
+      <div style={{display: 'none'}}>
         <InfoItem
           {...formItemLayout}
-          label={(<span>* <PrintGainLossLabel gainLoss={calculatorStep.sumGainLoss} /></span>)}
+          label={
+            <span>
+              * <PrintGainLossLabel gainLoss={calculatorStep.sumGainLoss} />
+            </span>
+          }
         >
           <div style={{textAlign: 'right', textDecoration: 'underline'}}>
             <PrintAsset
               mode={'text'}
               value={calculatorStep.sumIncome - calculatorStep.sumExpenses}
               asset={calculatorStep.residenceCurrency}
-              colors={colors}/>
+              colors={colors}
+            />
           </div>
         </InfoItem>
         {/* <div style={{textAlign: 'right'}}>* - <FormattedMessage id="CalculatorSummary.taxable" defaultMessage="taxable"/></div> */}
